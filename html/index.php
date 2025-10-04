7d97a1081c5379fb4a13d7976598f982b80d5e86
Ramaa <daniramadany2005@gmail.com>
2025-10-05 01:35:37 +0800

    Cleanup: Remove unused HTML, CSS files, and images from the project

---
+++ html/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Corinthia:wght@400;700&family=Pacifico&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="body">
+      <div style="text-align: center">
+      <div class="logo">Logo</div><br>
+      <div class="kotak">
+          <h1>Sign Up</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <form action=""></form> <!-- untuk makukkin nama label dll -->
+          <label for="">Name*</label><br>
+          <input type="text"required><br>
+          <label for="">Email*</label><br>
+          <input type="Email"required><br>
+          <label for="">Password*</label><br>
+          <input type="password"required><br>
+<div>
+
+<div class="button-box">
+  <!-- Tombol Sign up -->
+  <button class="btn-signup">Sign up</button>
+
+  <!-- Tombol Sign up with Google -->
+  <button class="btn-google">
+    <span class="g-icon">G</span>
+    <span>Sign up with Google</span>
+  </button>
+  <p>Already have an account? Log in.</p>
+</div>
+</body>
+</html>
